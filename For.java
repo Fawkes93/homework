941ac4bc0101e9130ab7875fa05424eb9fe29733
Igor <igrusu93@gmail.com>
2018-08-08 18:52:58 +0300

    added class with for

---
+++ For.java
@@ -0,0 +1,13 @@
+	public class For{
+
+	public static void main(String args[]){
+	int i=0;
+	for (i=0; i<8; i++){
+	if(i%2==0)
+
+	System.out.println("* * * * * * * *"    );
+	else
+	System.out.println(" * * * * * * * *"   );
+		}
+	}
+}
